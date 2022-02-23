ad335a86c783dd1df6f0fa4d151a25d374f35061
sellmant0622 <100149311+sellmant0622@users.noreply.github.com>
2022-02-23 11:01:02 -0500

    Add files via upload
    P1HW2

---
+++ P1HW2_SellmanTyrrell.py
@@ -0,0 +1,16 @@
+# The Calculation between the number of students to the number of slices
+# 2/14/2022
+# CTI-110 P1HW2 - Pizza Order
+# Tyrrell Sellman
+#
+
+students = int(input('How many students do you want to order pizza for?'))
+slices = students * 3
+pizzas = students / 2
+
+
+print("\n----Pizza Order--------")
+print('Number of Students: ', students)
+print("Pizza Slices Needed: ", slices)
+print("Pizzas Need : ", pizzas)
+print("-------------------------")
